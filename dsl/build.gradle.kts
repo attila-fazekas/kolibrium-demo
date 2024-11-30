40a511dc8945c007ff16e5a8d10a68f541f892a4
Attila Fazekas <fazekas.attila33@gmail.com>
2024-11-30 16:14:36 +0100

    Add more tests
    Signed-off-by: Attila Fazekas <fazekas.attila33@gmail.com>

--- dsl/build.gradle.kts
+++ dsl/build.gradle.kts
@@ -16,6 +16,7 @@ dependencies {
     implementation("dev.kolibrium:kolibrium-dsl:0.4.0")
     implementation("dev.kolibrium:kolibrium-selenium:0.4.0")
     implementation("io.kotest:kotest-assertions-core-jvm:5.9.1")
+    testImplementation("org.assertj:assertj-core:3.26.3")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.11.3")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.11.3")
 }
